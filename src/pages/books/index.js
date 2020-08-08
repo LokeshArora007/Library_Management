7ae78a4fa5203147a50985f3ed1716ad279e4308
Shiva Kumar <shivah21@gmail.com>
2020-08-08 09:26:45 +0100

    User table and test scripts are updated

--- src/pages/books/index.js
+++ src/pages/books/index.js
@@ -12,7 +12,7 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import {
   DynamicPageFormFields
 } from '../../components';
-import books from './books.json';
+import books from '../users/books.json';
 
 /** STYLES HERE * */
 const useStyles = makeStyles(() => ({
@@ -23,23 +23,43 @@ const getBooks = ()=> {
     return books;
 }
 
-const Books = (props) => {
+const DynamicPageAdd = (props) => {
   const classes = useStyles();
   const [pageType, setPageType] = React.useState('add');
   const [expandAddPolicy, setExpandAddPolicy] = React.useState(true);
+
   React.useEffect(() => {
     
   }, []);
+
   const books = getBooks();
+
   return (
-    <div>
-        <ul>
-        {books.map((book, i) => (
-            <li>{book.title}</li>
-        ))}
-        </ul>
-    </div>
+    <Grid container>
+      <Grid item xs={12} md={12} lg={12}>      
+        <ExpansionPanel
+          expanded={expandAddPolicy}
+          onChange={(e, exp) => setExpandAddPolicy(!expandAddPolicy)}
+        >
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography variant="h6" style={{ textTransform: 'capitalize' }}>
+              {pageType} Page
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <DynamicPageFormFields
+              pageType={pageType}
+              cancelClick={() => props.history.push('/pages')}
+            />
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </Grid>
+    </Grid>
   );
 };
 
-export default Books;
+export default DynamicPageAdd;

a85ffb20cde13384c21046a659e51598f2027ced
lokeshrora007 <er.lokesharora@gmail.com>
2020-08-08 13:52:34 +0530

    added the login unit test case

--- src/pages/login/login.test.js
+++ src/pages/login/login.test.js
@@ -1,32 +1,9 @@
 import React from 'react';
-import { createShallow } from '@material-ui/core/test-utils';
-import { createStore, StoreProvider } from 'easy-peasy';
-import model from '../../state/model';
+import { render } from '@testing-library/react';
 import SignInSide from './login';
 
-describe('Login Page', () => {
-  let shallow;
-  let wrapper;
-
-  beforeAll(() => {
-    shallow = createShallow({ dive: true, untilSelector: 'button' });
-  });
-
-  beforeEach(() => {
-    const store = createStore(model);
-    const app = (
-      <StoreProvider store={store}>
-        <SignInSide.WrappedComponent />
-      </StoreProvider>
-    );
-    wrapper = shallow(app);
-  });
-
-  it('should render page', () => {
-    expect(wrapper.length).toBe(1);
-  });
-
-  it('should render user textfield', () => {
-    console.log(wrapper);
-  });
+test('Check if Login Page is rendered', () => {
+  const { container, getByText } = render(<SignInSide />)
+  console.log(getByText('Login'));
+  expect(getByText('Login')).toBeInTheDocument();
 });
